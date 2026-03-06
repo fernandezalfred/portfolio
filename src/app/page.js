69262581d3f31fc35b83662c96c2e6aa27929671
Alfredo Fernandez <245248952+fernandezalfred@users.noreply.github.com>
2026-03-06 14:47:08 -0400

    format by prittier

--- src/app/page.js
+++ src/app/page.js
@@ -6,31 +6,35 @@ import ClientProjectView from "@/components/client-view/project";
 import Image from "next/image";
 
 async function extractAllDatas(currentSection) {
-  const res = await fetch(`http://localhost:3000/api/${currentSection}/get`,{
+  const res = await fetch(`http://localhost:3000/api/${currentSection}/get`, {
     method: "GET",
-    cache: "no-store"
+    cache: "no-store",
   });
   const data = await res.json();
   return data && data.data;
 }
 
 export default async function Home() {
-
   const homeSectionData = await extractAllDatas("home");
   const aboutSectionData = await extractAllDatas("about");
   const experienceSectionData = await extractAllDatas("experience");
   const educationSectionData = await extractAllDatas("education");
   const projectSectionData = await extractAllDatas("project");
- 
+
   return (
     <div>
       <ClientHomeView data={homeSectionData} />
-      <ClientAboutView data={ 
-        aboutSectionData && aboutSectionData.length ? aboutSectionData[0] : []
-      } />
-      <ClientExperienceAndEducationView  educationData={educationSectionData} experienceData={experienceSectionData}  />
+      <ClientAboutView
+        data={
+          aboutSectionData && aboutSectionData.length ? aboutSectionData[0] : []
+        }
+      />
+      <ClientExperienceAndEducationView
+        educationData={educationSectionData}
+        experienceData={experienceSectionData}
+      />
       <ClientProjectView data={projectSectionData} />
-      <ClientContactView/>
+      <ClientContactView />
     </div>
   );
 }
